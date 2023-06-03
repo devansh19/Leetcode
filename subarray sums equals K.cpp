32fbecf1916e980552b0296c930bc4a8e3e1afa8
Devansh Suri <33149887+devansh19@users.noreply.github.com>
2023-06-03 14:15:59 +0530

    Create subarray sums equals K.cpp
    Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
    
    A subarray is a contiguous non-empty sequence of elements within an array.

---
+++ subarray sums equals K.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+        int count=0,sum=0;
+        int n = nums.size();
+
+        for(int i=0;i<nums.size();i++)
+        {
+          sum = nums[i];
+
+          if(sum == k){
+              count++;
+          }
+
+          for(int j=i+1;j<nums.size();j++){
+             sum+= nums[j];
+
+             if(sum == k){
+                 count++;
+             }
+          }
+        }
+        return count;
+
+    }
+};
